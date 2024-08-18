771b265d2e37dc453f239cc276e4cf37a1e4854c
rehanshaikh25 <shakilrehan23@gmail.com>
2024-08-18 22:36:47 +0530

    added some info to the / route

--- routes/welcome.go
+++ routes/welcome.go
@@ -3,5 +3,11 @@ package routes
 import "github.com/gofiber/fiber/v2"
 
 func WelcomeRoute(c *fiber.Ctx) error {
-    return c.SendString("Welcome to the Fiber API!")
+    return c.SendString(`
+    Welcome to the Fiber API!
+    
+    info about the api @ - /info 
+    simple database fetch @ - /members
+    
+    `)
 }
